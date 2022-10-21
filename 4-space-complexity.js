7f502cd3b4a0dd9dc913cadc9bced04f50ab33d9
John Manack <74992804+john-manack@users.noreply.github.com>
2022-10-21 13:12:46 -0400

    updates variables in example 4

--- 4-space-complexity.js
+++ 4-space-complexity.js
@@ -1,7 +1,7 @@
 // space complexity (memory)
 
-const logBooForEachInput = (n) => {
-    for (let i = 0; i < n.length; i++) { // assigned variable 'let i = 0', O(1)
+const logBooForEachInput = (array) => {
+    for (let i = 0; i < array.length; i++) { // assigned variable 'let i = 0', O(1)
         console.log('boo!')
     }
 }
@@ -9,11 +9,11 @@ const logBooForEachInput = (n) => {
 logBooForEachInput([1, 2, 3, 4, 5]); // Space complexity = O(1), because the only memory used was assigning the variable
 
 const arrayOfBooNTimes = (n) => {
-    let hiArray = []; // created data structure, O(1)
+    let booArray = []; // created data structure, O(1)
     for (let i = 0; i < n; i++) { // assigned variable, O(1)
-        hiArray[i] = 'hi' // filled data structure with 'n' loops, O(n)
+        booArray[i] = 'boo' // filled data structure with 'n' loops, O(n)
     }
-    return hiArray;
+    return booArray;
 }
 
 console.log(arrayOfBooNTimes(6)); // Space Complexity = O(n)
