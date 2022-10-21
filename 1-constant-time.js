51c9d72a30ae87b0400dd4d9aebfd3aaa4434e97
John Manack <74992804+john-manack@users.noreply.github.com>
2022-10-21 10:01:02 -0400

    updates example 1

--- 1-constant-time.js
+++ 1-constant-time.js
@@ -1,16 +1,16 @@
 // "Constant time" O(1) examples
 
-const boxes = [0, 1, 2, 3, 4, 5];
+const boxes = ['red', 'blue', 'green', 'yellow'];
 
 // Example 1
-function logFirstBox(boxArray) {
-  console.log(boxArray[0]); // O(1)
+function logFirstBox(array) {
+  console.log(array[0]); // O(1)
 }
-logFirstBox(boxArray[0]) // O(1) --> Constant Time
+logFirstBox(boxes) // O(1) --> Constant Time
 
 // Example 2
-function logFirstTwoBoxes(boxArray) {
-  console.log(boxArray[0]); // O(1)
-  console.log(boxArray[1]); // O(1)
+function logFirstTwoBoxes(array) {
+  console.log(array[0]); // O(1)
+  console.log(array[1]); // O(1)
 }
-logFirstTwoBoxes(boxes); // in total, runs O(2), but we drop constants (or redue to 1): O(1) - "Constant time"
+logFirstTwoBoxes(boxes); // in total, runs O(2), but we drop constants (or reduce to 1): O(1) - "Constant time"
