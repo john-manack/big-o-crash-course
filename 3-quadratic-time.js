814b83ea845c0a3d4cf3a16ecd149684421077dd
John Manack <74992804+john-manack@users.noreply.github.com>
2022-10-21 10:08:16 -0400

    updtes example 3

--- 3-quadratic-time.js
+++ 3-quadratic-time.js
@@ -1,8 +1,8 @@
 // "Quadratic Time" - O(n^2) example
 
-// Log all pairs of array
+// Log all pairs of array ([1,1], [1,2], [1,3], etc..)
 
-const array = [1, 2, 3, 4, 5, 5];
+const array = [1, 2, 3, 4, 5];
 
 const logAllPairs = (integerArray) => {
   integerArray.forEach((ints) => { // O(n)
